13b947c50d40a715212599d4576cf431055cf3c2
Paulo César <pdesignerpb@gmail.com>
2020-05-23 17:26:40 -0300

    User can login and edit some information

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -11,6 +11,28 @@ class UserController {
 
     return res.json({ id, name, email, provider });
   }
+
+  async update(req, res) {
+    const { email, oldpassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists)
+        return res.status(400).json({ error: 'User already exists' });
+    }
+
+    if (oldpassword && !(await user.checkPassword(oldpassword)))
+      return res.status(401).json({ error: 'Password does not match' });
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
 }
 
 export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'af9605508d1ac8a1ae27d47616c58332',
+  expiresIn: '7d',
+};
