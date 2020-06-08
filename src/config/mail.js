8196976683907acca41ca695acd14023029b5e9e
martins20 <pdesignerpb@gmail.com>
2020-06-08 06:49:59 -0300

    Feature: when an user cancel an appointment, the provider receive an email to notify it.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1ad535261baa80',
+    pass: 'fb3fe46944bd2d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
