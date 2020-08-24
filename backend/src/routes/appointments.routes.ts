ed0d4bc04c273a8bb96edcb8b6b59e58a909e1c8
martins20 <pdesignerpb@gmail.com>
2020-08-24 20:03:58 -0300

    Restructure of post route appointment

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,18 +1,27 @@
 import { Router } from "express";
-import { uuid } from "uuidv4";
+import { startOfHour, parseISO, isEqual } from "date-fns";
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+import Appointment from "../models/appointmens";
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post("/", (request, response) => {
     const { provider, date } = request.body;
 
-    const appointment = {
-        id: uuid(),
-        provider,
-        date,
-    };
+    const parsedDate = startOfHour(parseISO(date));
+
+    const findAppointmentInSameDate = appointments.find((appointment) =>
+        isEqual(parsedDate, appointment.date)
+    );
+
+    if (findAppointmentInSameDate)
+        return response.json({
+            message: "This appointmenr is already booked",
+        });
+
+    const appointment = new Appointment(provider, parsedDate);
 
     appointments.push(appointment);
 
