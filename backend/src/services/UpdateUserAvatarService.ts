169b97d5f10e231ca6f1ce8bfeb218e35f31ce83
martins20 <pdesignerpb@gmail.com>
2020-08-29 16:32:49 -0300

    User can save you avatar file on DB and storage it on disk ( deleting exist file on disk )

---
+++ backend/src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,49 @@
+import { getRepository } from "typeorm";
+import { join } from "path";
+
+import { promises, unlink } from "fs";
+
+import uploadConfig from "../config/upload";
+import User from "../models/User";
+
+interface Request {
+    user_id: string;
+    avatarFileName: string;
+}
+
+class UpdateUserAvatarService {
+    async execute({ user_id, avatarFileName }: Request): Promise<User> {
+        const UsersRepository = getRepository(User);
+
+        const user = await UsersRepository.findOne(user_id);
+
+        if (!user) {
+            throw new Error("Only authenticated users can change avatar");
+        }
+
+        if (user.avatar) {
+            //Deletar avatar
+
+            const userAvatarFilePath = join(
+                uploadConfig.directory,
+                user.avatar
+            );
+
+            const userAvatarFileExists = await promises.stat(
+                userAvatarFilePath
+            );
+
+            if (userAvatarFileExists) {
+                await promises.unlink(userAvatarFilePath);
+            }
+        }
+
+        user.avatar = avatarFileName;
+
+        await UsersRepository.save(user);
+
+        return user;
+    }
+}
+
+export default UpdateUserAvatarService;
