2b60d3f521fb5f0a69b0dbf8265e0790ea9e8881
martins20 <pdesignerpb@gmail.com>
2021-01-23 20:38:23 -0300

    Test: implement test of Autentication

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,66 @@
+import AppError from '@shared/errors/AppError';
+
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+
+let authenticateUser: AuthenticateUserService;
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let createUser: CreateUserService;
+
+describe('AuthenticateUser', () => {
+    beforeEach(() => {
+        fakeUsersRepository = new FakeUsersRepository();
+        fakeHashProvider = new FakeHashProvider();
+        authenticateUser = new AuthenticateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
+        createUser = new CreateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
+    });
+
+    it('should be able to Authenticate ', async () => {
+        const user = await createUser.execute({
+            name: 'Jonh Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        const response = await authenticateUser.execute({
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        expect(response).toHaveProperty('token');
+        expect(response.user).toEqual(user);
+    });
+
+    it('should not be able to Authenticate with non existent user', async () => {
+        expect(
+            authenticateUser.execute({
+                email: 'johndoe@example.com',
+                password: '123456',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to Authenticate with wrong password', async () => {
+        await createUser.execute({
+            name: 'Jonh Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        expect(
+            authenticateUser.execute({
+                email: 'johndoe@example.com',
+                password: 'wrong-password',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,4 +1,3 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { injectable, inject } from 'tsyringe';
 
@@ -7,6 +6,7 @@ import AppErro from '@shared/errors/AppError';
 
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
     email: string;
@@ -23,6 +23,9 @@ class AuthenticateUserService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('HashProvider')
+        private hashProvider: IHashProvider,
     ) {}
 
     async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -32,7 +35,10 @@ class AuthenticateUserService {
             throw new AppErro('Incorrect email/password combinations.', 401);
         }
 
-        const passwordMatched = await compare(password, user.password);
+        const passwordMatched = await this.hashProvider.compareHash(
+            password,
+            user.password,
+        );
 
         if (!passwordMatched) {
             throw new AppErro('Incorrect email/password combinations.', 401);
