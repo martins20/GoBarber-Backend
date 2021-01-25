cbfcf5ecf1550331b516704c769f52a34bfa858b
martins20 <pdesignerpb@gmail.com>
2021-01-25 17:38:30 -0300

    Feat: Send token to user email.

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,7 +1,5 @@
 import { Router } from 'express';
 
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
-
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentController from '../controllers/AppointmentsController';
 
@@ -9,14 +7,6 @@ const appointmentsRouter = Router();
 const appointmentsController = new AppointmentController();
 
 appointmentsRouter.use(ensureAuthenticated);
-
-// appointmentsRouter.get('/', async (request, response) => {
-//     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-
-//     const appointments = await appointmentsRepository.find();
-
-//     return response.json(appointments);
-// });
 
 appointmentsRouter.post('/', appointmentsController.create);
 
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,15 +1,17 @@
 import { container } from 'tsyringe';
-// import IMailProvider from './MailProvider/models/IMailProvider';
 
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
+
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
     'StorageProvider',
     DiskStorageProvider,
 );
 
-// container.registerSingleton<IMailProvider>(
-//     'MailProvider',
-//     ,
-// );
+container.registerInstance<IMailProvider>(
+    'MailProvider',
+    new EtherealMailProvider(),
+);
