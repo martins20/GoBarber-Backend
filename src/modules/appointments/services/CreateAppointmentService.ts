c964cb681251c4e1bd8877f01e8c2dda21825996
martins20 <martins20.tech@gmail.com>
2021-06-19 00:05:01 -0300

    feat: create a notification to provider when user create a new appointment

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,7 +1,9 @@
-import { startOfHour, isBefore, getHours } from 'date-fns';
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -16,6 +18,9 @@ class CreateAppointmentService {
     constructor(
         @inject('AppointmentsRepository')
         private AppointmentsRepository: IAppointmentsRepository,
+
+        @inject('NotificationsRepository')
+        private NotificationsRepository: INotificationsRepository,
     ) {}
 
     async execute({
@@ -47,6 +52,16 @@ class CreateAppointmentService {
             provider_id,
             user_id,
             date: appointmentDate,
+        });
+
+        const dateFormated = format(
+            appointmentDate,
+            `dd/MM/yyyy 'às' HH:mm'h'`,
+        );
+
+        await this.NotificationsRepository.create({
+            recipient_id: provider_id,
+            content: `Novo agendamento para ${dateFormated}`,
         });
 
         return appointment;
